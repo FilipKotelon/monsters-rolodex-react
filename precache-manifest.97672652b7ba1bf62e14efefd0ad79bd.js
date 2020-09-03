dac9d8a0f03c506970a845a9f7a71409f3223a85
Filip <off.f.kotelon@gmail.com>
2020-09-03 12:27:39 +0200

    Updates

---
+++ precache-manifest.97672652b7ba1bf62e14efefd0ad79bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842a28f9289e39c4660252819abe8fe5",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "7bbde10d6452aac8f1fd",
+    "url": "/monsters-rolodex-react/static/css/main.3e566fc5.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex-react/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bbde10d6452aac8f1fd",
+    "url": "/monsters-rolodex-react/static/js/main.d4ebc2f0.chunk.js"
+  },
+  {
+    "revision": "18908f6eb88b51269a6a",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.b428d943.js"
+  }
+]);
